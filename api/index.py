2f85a950910707acf805fca174278089064ad70a
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-12-18 09:46:48 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,34 +1,60 @@
-from os import *
+from telebot import TeleBot
+from threading import Thread
+import os
 
-system("pip install time")
+text1 = ""
+text2 = ""
+text3 = ""
+text6 = ""
 
-from flask import Flask, Response
-from time import *
+def bombs(number, id):
+    global text1
+    while True:
+        with open(f"{id}.txt", "r") as file:
+            text1 = file.read()
+        if text1 == "Start":
+            # Здесь должен быть код для обработки запуска сервисов, но он отсутствует в предоставленном фрагменте кода.
+            pass
 
-num = 0
-text = ""
+bot = TeleBot("6705628312:AAGmQ-CSIVZ9xQE-vnOkix6zOjM_mNBdhaA")
 
-app = Flask(__name__)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, """☁ Добро Пожаловать ☁
+    Вы Находитесь В 💣 Бомбере 💣
+    Тут Вы Можете Наказать Своего Обидчика И Друга
+    Для Того Чтобы Узнать Больше Введите /help""")
 
-@app.route("/<path:message>")
-def echo(message):
-    global num
-    global text
-    text = message + "\n"
-    num = 1
-    return "OK"
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, """★ /bomb (Номер Телефона) 
+    ★ /status Посмотреть Свой Статус 
+    ★ /buy Купить Премиум""")
 
-@app.route("/stream")
-def stream():
-    def chat():
-        global num
-        global text
-        while True:
-            sleep(1)
-            if num == 1:
-                num = 0
-                yield text
-    return Response(chat(), mimetype="text/event-stream")
+@bot.message_handler(commands=['buy'])
+def buy(message):
+    bot.send_message(message.chat.id, "Напишите нам @progress135top")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@bot.message_handler(commands=['bomb'])
+def bomb(message):
+    global text1
+    global text2
+    global text3
+    global text6
+    text6 = os.path.abspath(f"{message.chat.id}.txt")
+    with open(f"{message.chat.id}.txt", "w") as file2:
+        file2.write("Start")
+    text2 = message.text
+    text3 = text2.split(' ')
+    start = Thread(target=bombs, args=(" ".join(text3[1:2]), " ".join(text3[2:3])))  
+    start.start()
+
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    global text6
+   text6 = os.path.abspath(f"{message.chat.id}.txt")
+    with open(f"{message.chat.id}.txt", "w") as file4:
+        file4.write("Stop")
+    bot.send_message(message.chat.id, "Атака окончена!")
+            
+bot.polling(none_stop=True)
